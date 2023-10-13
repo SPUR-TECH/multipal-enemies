37bdbfa557a1eb941615ec826efa4dbebbff82ee
Scott Atkinson <scott.atkin79@gmail.com>
2023-10-13 13:44:11 +0000

    added random curve to ghost movements

--- script.js
+++ script.js
@@ -84,17 +84,18 @@ window.addEventListener('load', function () {
             this.width = this.spriteWidth / 2;
             this.height = this.spriteHeight / 2;
             this.x = this.game.width;
-            this.y = Math.random() * this.game.height * 0.6;
+            this.y = Math.random() * this.game.height * 0.4;
             this.markedForDeletion = false;
             this.image = ghost; //  Id name
             this.vx = Math.random() * 0.2 + 0.1;
             this.angle = 0;
+            this.curve = Math.random() * 3
 
             console.log(this.image);
         }
         update(deltaTime) {
             super.update(deltaTime)
-            this.y += Math.sin(this.angle) * 1;
+            this.y += Math.sin(this.angle) * this.curve;
             this.angle += 0.04;
         }
         draw() {
